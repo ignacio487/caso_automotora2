eb5f85a45b0f080cfc5bd5cf07caa82232fd37bd
ignacio487 <i.riquelme10@ufromail.cl>
2023-09-25 18:01:01 -0300

    tarea caso automotora 2

---
+++ src/main/java/RegistroVehiculos.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class RegistroVehiculos {
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+}
