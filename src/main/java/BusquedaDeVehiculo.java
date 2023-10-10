6aad0af880d5da8eaea7e97e11f9b48286ce0fc6
ignacio487 <i.riquelme10@ufromail.cl>
2023-10-10 01:55:46 -0300

    tarea caso automotora 2

--- src/main/java/BusquedaDeVehiculo.java
+++ src/main/java/BusquedaDeVehiculo.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class BusquedaDeVehiculo {
+public class BusquedaDeVehiculo extends JFrame{
     private JTextField textField1;
     private JComboBox comboBox1;
     private JButton buscarVehiculoBt;
--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,8 +1,15 @@
 package principal;
+import vista.VentanaAutomotora;
+import javax.swing.*;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		throw new UnsupportedOperationException();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				VentanaAutomotora ventanaAutomotora = new VentanaAutomotora();
+				ventanaAutomotora.setVisible(true);
+			}
+		});
 	}
-}
+	}
